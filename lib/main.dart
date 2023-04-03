8763a8d3c5cf312601db7b1004611197fbf61e6e
Anmol <anmol.sin@gmail.com>
2023-04-03 14:23:46 +0530

    Change to Schedule notification

--- lib/main.dart
+++ lib/main.dart
@@ -12,15 +12,15 @@ void main() {
         channelName: 'Basic Notifications',
         channelDescription: 'channel_Description',
         importance: NotificationImportance.Max,
-        defaultPrivacy: NotificationPrivacy.Secret,
+        // defaultPrivacy: NotificationPrivacy.Secret,
         defaultRingtoneType: DefaultRingtoneType.Notification,
         enableVibration: true,
         defaultColor: Colors.redAccent,
         channelShowBadge: true,
         enableLights: true,
-        icon: 'resource://drawable/res_naruto',
-        playSound: true,
-        soundSource: 'resource://raw/naruto_jutsu',
+        // icon: 'resource://drawable/res_naruto',
+        // playSound: true,
+        // soundSource: 'resource://raw/naruto_jutsu',
       ),
     ],
     debug: true,
@@ -64,7 +64,7 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  triggerNotification() {
+  scheduleNotification() async {
     AwesomeNotifications().createNotification(
       content: NotificationContent(
         id: 10,
@@ -74,7 +74,27 @@ class _MyHomePageState extends State<MyHomePage> {
             "https://images.unsplash.com/photo-1555485898-0f23a85a607f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
         notificationLayout: NotificationLayout.BigPicture,
       ),
+      /// Permission added to androidManifest for scheduling
+      schedule: NotificationCalendar.fromDate(
+        date: DateTime.now().add(
+          const Duration(seconds: 30),
+        ),
+        preciseAlarm: true,
+        // Battery hungry
+        allowWhileIdle: true,
+        // will bypass low battery
+        repeats: true,
+      ),
+      /// Repeat
+      // schedule: NotificationCalendar(
+      //   second: 0,
+      //   repeats: true,
+      // ),
     );
+  }
+
+  cancelScheduleNotification(int id) async {
+    await AwesomeNotifications().cancelSchedule(id);
   }
 
   @override
@@ -84,9 +104,18 @@ class _MyHomePageState extends State<MyHomePage> {
         title: const Text('Notification'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: triggerNotification,
-          child: const Text('Trigger Notification'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: scheduleNotification,
+              child: const Text('Schedule Notification'),
+            ),
+            ElevatedButton(
+              onPressed: () => cancelScheduleNotification(10),
+              child: const Text('Cancel Schedule Notification'),
+            ),
+          ],
         ),
       ),
     );
